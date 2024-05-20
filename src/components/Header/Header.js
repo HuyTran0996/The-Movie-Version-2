64303fb917f193782a8dfb811fff8e3a42d0e132
HuyTran0996 <tranviquanghuy@gmail.com>
2024-05-20 11:37:17 +0700

    2024-05-20 add header footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 import "./Header.scss";
 
@@ -8,6 +9,7 @@ import { MdHomeFilled } from "react-icons/md";
 import { IoSearchOutline } from "react-icons/io5";
 import { PiTelevisionFill } from "react-icons/pi";
 import { BiSolidMoviePlay } from "react-icons/bi";
+import { BiSolidCameraMovie } from "react-icons/bi";
 
 import userIcon from "../../assets/user.png";
 import logo from "../../assets/logo.png";
@@ -15,9 +17,16 @@ import { showToast } from "../../components/ToastMessage";
 
 const Header = () => {
   const location = useLocation();
+  const [isMobile, setIsMobile] = useState(false);
   const [input, SetInput] = useState("");
   const [page, setPage] = useState("");
   const [toggleMenu, setToggleMenu] = useState(false);
+
+  const isPhoneScreen = useMediaQuery({ query: "(max-width: 600px)" });
+
+  useEffect(() => {
+    setIsMobile(isPhoneScreen);
+  }, [isPhoneScreen]);
 
   const handleNavbar = (e) => {
     e.preventDefault();
@@ -32,48 +41,86 @@ const Header = () => {
     }
     SetInput("");
   };
+
+  useEffect(() => {
+    const setActiveClass = () => {
+      let currentPage = location.pathname;
+      if (currentPage === "/") {
+        setPage("home");
+      }
+      if (currentPage.includes("/tv")) {
+        setPage("tv");
+      }
+      if (currentPage.includes("/movie")) {
+        setPage("movie");
+      }
+    };
+    setActiveClass();
+  }, [location]);
   return (
     <header className="header">
       <Link to="/">
-        <img src={logo} alt="logo" width={120} />
+        {isMobile ? (
+          <BiSolidCameraMovie size={35} className="logo" />
+        ) : (
+          <img className="logo" src={logo} alt="logo" width={120} />
+        )}
       </Link>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="searchForm">
         <input
           type="text"
           placeholder="Search here..."
           value={input}
           onChange={(e) => SetInput(e.target.value)}
+          className="inputBox"
         />
-        <button type="submit">
-          <IoSearchOutline />
+        <button type="submit" className="searchIcon">
+          <IoSearchOutline size={25} />
         </button>
       </form>
 
       <button
         type="button"
-        className="navbar-toggle-btn"
+        className="navbar-toggler-btn"
         onClick={handleNavbar}
       >
         <HiOutlineMenuAlt3
           size={35}
           style={{
-            color: `${toggleMenu ? "#010101" : "#fff"}`,
+            color: `${toggleMenu ? "orange" : "#fff"}`,
           }}
         />
       </button>
-      <nav onClick={handleNavbar}>
-        <ul>
-          <Link to="/">
-            <MdHomeFilled />
+      <nav
+        onClick={handleNavbar}
+        className={toggleMenu ? "navbar show-navbar-collapse" : "navbar"}
+      >
+        <ul className="navbar-nav">
+          <Link
+            to="/"
+            className={`${page === "home" ? "active" : ""} nav-link`}
+            style={{ "--i": 1 }}
+          >
+            <MdHomeFilled size={25} />
             Home
           </Link>
-          <Link to="/tv">
-            <PiTelevisionFill />
+
+          <Link
+            to="/tv"
+            className={`${page === "tv" ? "active" : ""} nav-link`}
+            style={{ "--i": 2 }}
+          >
+            <PiTelevisionFill size={25} />
             TV Shows
           </Link>
-          <Link to="/movie">
-            <BiSolidMoviePlay />
+
+          <Link
+            to="/movie"
+            className={`${page === "movie" ? "active" : ""} nav-link`}
+            style={{ "--i": 3 }}
+          >
+            <BiSolidMoviePlay size={25} />
             Movies
           </Link>
         </ul>
